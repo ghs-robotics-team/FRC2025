09db7c8bbfa138fec315b42b7d76cd7d88a60c0b
DaneWenzel <dane.wenzel699@gsdstudents.org>
2025-02-08 15:10:12 -0600

    Very Basic Arm and Elevator Subsystems.

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+  /** Creates a new Elevator. */
+  SparkMax topHand = new SparkMax(2, MotorType.kBrushless); //Get ID
+  SparkMax bottomHand = new SparkMax(3, MotorType.kBrushless);
+  SparkMax armMover = new SparkMax(4, MotorType.kBrushless);
+  PIDController pid;
+  public Arm() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.pid = new PIDController (0.35,0,0.0005); 
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    double P = SmartDashboard.getNumber("Indexer-P", 0.28); 
+    double I = SmartDashboard.getNumber("Indexer-I", 0.0);
+    double D = SmartDashboard.getNumber("Indexer-D", 0.0005);
+    double Speed =SmartDashboard.getNumber("Arm Speed", 6.5);
+
+    // Set PID numbers
+    pid.setP(P);
+    pid.setI(I);
+    pid.setD(D);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+  /** Creates a new Elevator. */
+  SparkMax Left = new SparkMax(0, MotorType.kBrushless); //Get ID
+  SparkMax Right = new SparkMax(1, MotorType.kBrushless);
+  PIDController pid;
+  public Elevator() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.pid = new PIDController (0.35,0,0.0005); 
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    double P = SmartDashboard.getNumber("Indexer-P", 0.28); 
+    double I = SmartDashboard.getNumber("Indexer-I", 0.0);
+    double D = SmartDashboard.getNumber("Indexer-D", 0.0005);
+    double Speed =SmartDashboard.getNumber("Arm Speed", 6.5);
+
+    // Set PID numbers
+    pid.setP(P);
+    pid.setI(I);
+    pid.setD(D);
+  }
+}
