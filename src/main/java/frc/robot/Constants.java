1fab4bf915bc83286b3d859d92d33935518b310b
DaneWenzel <dane.wenzel699@gsdstudents.org>
2025-03-10 18:36:03 -0500

    Updated Libraries, Changed some constants and PID values. (Got rid of speed limit)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,12 +19,12 @@ import swervelib.math.Matter;
  */
 public final class Constants {
 
-  public static final double ROBOT_MASS = (89.8) * 0.453592; // 32lbs * kg per pound
+  public static final double ROBOT_MASS = (95) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED = 5; // 0.2/2.0 Min/Max
+  public static final double MAX_SPEED = 6; // 0.2/2.0 Min/Max
 
-  public static final class SetPointConstants { //Dummy Values until measured
+  public static final class SetPointConstants { 
     public static final double ARM_LEFT_LOW = 3984.258;
     public static final double ARM_LEFT_MIDDLE = 3520.898;
     public static final double ARM_LEFT_HIGH = 3978.633;
